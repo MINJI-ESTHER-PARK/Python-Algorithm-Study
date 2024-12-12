95b5d9754feb054fac681cc10a16afccb9d0638b
MINJI-ESTHER-PARK <mj.esther.p@gmail.com>
2024-12-12 22:26:18 +0900

    first commit

---
+++ Chapter03/Chapter03-01.py
@@ -0,0 +1,24 @@
+n, m, k = map(int, input().split())
+
+num_list = []
+for i in range(n):
+    num_list.append(input())
+
+print(num_list)
+
+num_list.sort(reverse=True)
+num_list
+
+num_1 = int(num_list[0])
+num_2 = int(num_list[1])
+
+result = 0
+
+for i in range(m):
+    result += num_1
+    k -= 1
+    if k == 0:
+        result += num_2
+        k == i - 1
+
+result
